61531d60a5082c64b1885eb72b618d38eebdba31
BerkeTunaB <berketuna.bostanci@gmail.com>
2023-07-08 19:57:59 +0300

    =Merhaba

--- Assets/Yigit.cs
+++ Assets/Yigit.cs
@@ -7,7 +7,7 @@ public class Berke : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //merhaba bu kod bozuk
     }
 
     // Update is called once per frame
