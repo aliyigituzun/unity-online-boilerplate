08855824f58eb1534478c12b40d0302ed0d04deb
CodingSodaGuy <92642031+CodingSodaGuy@users.noreply.github.com>
2023-07-09 18:44:31 +0300

    =Update

---
+++ Assets/Scripts/NetworkManagerUI.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NetworkManagerUI : MonoBehaviour
+{
+    [SerializeField] private Button serverBtn;
+    [SerializeField] private Button hostBtn;
+    [SerializeField] private Button clientBtn;
+
+
+}
