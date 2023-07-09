b6be56bdc537c0a2bfa3cbc58ae4371364da69ca
CodingSodaGuy <92642031+CodingSodaGuy@users.noreply.github.com>
2023-07-09 19:39:35 +0300

    =Updates

--- Assets/Scripts/NetworkManagerUI.cs
+++ Assets/Scripts/NetworkManagerUI.cs
@@ -1,13 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class NetworkManagerUI : MonoBehaviour
 {
+
+
     [SerializeField] private Button serverBtn;
     [SerializeField] private Button hostBtn;
     [SerializeField] private Button clientBtn;
+
+    private void Awake()
+    {
+        serverBtn.onClick.AddListener(() => {
+            NetworkManager.Singleton.StartServer();
+        });
+        hostBtn.onClick.AddListener(() => {
+            NetworkManager.Singleton.StartHost();
+        });
+        clientBtn.onClick.AddListener(() => {
+            NetworkManager.Singleton.StartClient();
+        });
+    }
 
 
 }
