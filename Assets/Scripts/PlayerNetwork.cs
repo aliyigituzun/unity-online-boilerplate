0260e911db3c188be61ca2420a39f5c009b3ea59
CodingSodaGuy <92642031+CodingSodaGuy@users.noreply.github.com>
2023-07-10 10:13:32 +0300

    =Updates

--- Assets/Scripts/PlayerNetwork.cs
+++ Assets/Scripts/PlayerNetwork.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class NetworkPlayer : MonoBehaviour
+
+public class NetworkPlayer : NetworkBehaviour
 {
     void Update() {
         Vector3 moveDir = new Vector3(0, 0, 0);
 
-        if (Input.GetKeyDown(KeyCode.W)) moveDir.z += +1f;
-        if (Input.GetKeyDown(KeyCode.S)) moveDir.z += -1f;
-        if (Input.GetKeyDown(KeyCode.A)) moveDir.x += -1f;
-        if (Input.GetKeyDown(KeyCode.D)) moveDir.x += +1f;
+        if (Input.GetKeyDown(KeyCode.W)) moveDir.z += +10f;
+        if (Input.GetKeyDown(KeyCode.S)) moveDir.z += -10f;
+        if (Input.GetKeyDown(KeyCode.A)) moveDir.x += -10f;
+        if (Input.GetKeyDown(KeyCode.D)) moveDir.x += +10f;
+
+        float moveSpeed = 30f;
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
     }
 }
