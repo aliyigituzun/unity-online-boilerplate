131b51c2e0700f192e605e0b2d2229aa5aeafa2a
CodingSodaGuy <92642031+CodingSodaGuy@users.noreply.github.com>
2023-07-11 01:26:31 +0300

    =Rpc additions

--- Assets/Scripts/PlayerNetwork.cs
+++ Assets/Scripts/PlayerNetwork.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using Unity.Netcode;
 using UnityEngine;
 
@@ -7,17 +8,48 @@ using UnityEngine;
 public class NetworkPlayer : NetworkBehaviour
 {
 
-    private NetworkVariable<int> randomNumber = new NetworkVariable<int>(1, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+    private NetworkVariable<MyCustomData> randomNumber = new NetworkVariable<MyCustomData>(
+        new MyCustomData
+        {
+            _int = 56,
+            _bool = true
+        }, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
 
-    
-        void Update() {
-        Debug.Log(OwnerClientId + "" + randomNumber.Value);
+    public struct MyCustomData : INetworkSerializable
+    {
+        public int _int;
+        public bool _bool;
+        public FixedString128Bytes message;
+
+        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+        {
+            serializer.SerializeValue(ref _int);
+            serializer.SerializeValue(ref _bool);
+        }
+    }
+    public override void OnNetworkSpawn()
+    {
+        randomNumber.OnValueChanged += (MyCustomData previousValue, MyCustomData newValue) =>
+        {
+            Debug.Log(OwnerClientId + " ; " + newValue._int + "; " + newValue._bool + " ; " + newValue.message);
+        };
+    }
+
+    void Update() {
+
+        if (!IsOwner) return;
 
         if (Input.GetKeyDown(KeyCode.T))
         {
-            randomNumber.Value = Random.Range(0, 100);
+            TestClientRpc(new ClientRpcParams { Send = new ClientRpcSendParams { TargetClientIds = new List<ulong> {0} } });
+            randomNumber.Value = new MyCustomData
+            {
+                _int = 10,
+                _bool = false,
+                message = "Your message"
+            };
         }
-            if (!IsOwner) return;
+        
         Vector3 moveDir = new Vector3(0, 0, 0);
 
         if (Input.GetKeyDown(KeyCode.W)) moveDir.z += +10f;
@@ -27,5 +59,16 @@ public class NetworkPlayer : NetworkBehaviour
 
         float moveSpeed = 30f;
         transform.position += moveDir * moveSpeed * Time.deltaTime;
+    }
+
+    [ServerRpc]
+    private void TestServerRpc(ServerRpcParams serverRpcParams)
+    {
+        Debug.Log("Serverito" + OwnerClientId);
+    }
+    [ClientRpc]
+    private void TestClientRpc(ClientRpcParams clientRpcParams)
+    {
+        Debug.Log("Cliento" + OwnerClientId);
     }
 }
